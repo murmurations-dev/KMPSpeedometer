bc9890ae4886a5a4b5be3a6673f5885124cc4f7e
Etienne Vautherin <etienne@vautherin.com>
2023-10-19 10:32:41 +0200

    ContentView

--- iosSpeedometer/iosSpeedometer/ContentView.swift
+++ iosSpeedometer/iosSpeedometer/ContentView.swift
@@ -1,16 +1,27 @@
 import SwiftUI
-import Speedometer
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
-	var body: some View {
-		Text(greet)
-	}
+    var model = ViewModel(initialState: .stopped)
+    
+    var body: some View {
+        VStack {
+            Text("model.speed")
+            
+            Button("Start") {
+                model.start()
+            }
+            .disabled(model.started == .started)
+            
+            Button("Stop") {
+                model.stop()
+            }
+            .disabled(model.started == .stopped)
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
